d1ba619f8454ac128dacc1026d6695c33e372e91
CarlieHamilton <carliemhamilton@gmail.com>
2021-01-09 15:04:38 +1000

    extracted out prefix and added to readme

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # BOT
+Discord server bot that does all the things.
 
 ## Installation
 
-* copy the `sample.config.json` file to `auth.json` and add the bot's token
+* copy the `sample.config.json` file to `config.json` and fill in the details.
+
+
+## To Run Bot
+
+To run the bot locally, run `node index.js`
+
+## Using the Bot
+
+The bot's prefix is defined in `config.js`. In these docs it is assumed to be "!".
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
-const prefix = "!";
+const prefix = config.PREFIX;
 
 // Bot connects to server
 client.on('ready', () => {
@@ -14,6 +14,13 @@ client.on("message", (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   message.reply(`Carlie is the most awesomest person ever`);
+});
+
+// Welcome message to new member to server
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
 });
 
 client.login(config.BOT_TOKEN);
